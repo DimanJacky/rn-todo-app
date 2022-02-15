4a34a3b5acfef913bf16089d7252a1c21c4f7bb5
dmaslov <dmaslov@neoflex.ru>
2022-02-15 22:13:00 +0300

    context

--- src/MainLayout.js
+++ src/MainLayout.js
@@ -1,86 +1,67 @@
-import React, { useState, useContext } from 'react'
-import { View, StyleSheet } from 'react-native'
-import { Navbar } from './components/Navbar'
-import { THEME } from './theme'
-import { MainScreen } from './screens/MainScreen'
-import { TodoScreen } from './screens/TodoScreen'
-import { TodoContext } from './context/todo/todoContext'
+import React, {useState, useContext} from 'react'
+import {View, StyleSheet} from "react-native";
+import {Navbar} from "./components/Navbar";
+import {THEME} from "./theme";
+import {MainScreen} from "./screens/MainScreen";
+import {TodoScreen} from "./screens/TodoScreen";
+import {TodoContext} from "./context/todo/todoContext";
+import {ScreenContext} from './context/screen/screenContext';
 
 export const MainLayout = () => {
-  const todoContext = useContext(TodoContext)
-  const [todoId, setTodoId] = useState(null)
-  const [todos, setTodos] = useState([])
+  const {todos, addTodo, removeTodo, updateTodo} = useContext(TodoContext)
+    const {todoId, changeScreen} = useContext(ScreenContext)
 
-  const addTodo = title => {
-    setTodos(prev => [
-      ...prev,
-      {
-        id: Date.now().toString(),
-        title
-      }
-    ])
-  }
-
-  const removeTodo = id => {
-    const todo = todos.find(t => t.id === id)
-    Alert.alert(
-      'Удаление элемента',
-      `Вы уверены, что хотите удалить "${todo.title}"?`,
-      [
-        {
-          text: 'Отмена',
-          style: 'cancel'
-        },
-        {
-          text: 'Удалить',
-          style: 'destructive',
-          onPress: () => {
-            setTodoId(null)
-            setTodos(prev => prev.filter(todo => todo.id !== id))
-          }
-        }
-      ],
-      { cancelable: false }
-    )
-  }
-
-  const updateTodo = (id, title) => {
-    setTodos(old =>
-      old.map(todo => {
-        if (todo.id === id) {
-          todo.title = title
-        }
-        return todo
-      })
-    )
-  }
+  // const removeTodo = id => {
+  //   const todo = todos.find(t => t.id === id)
+  //   Alert.alert(
+  //       'Удаление элемента',
+  //       `Вы уверены, что хотите удалить "${todo.title}"?`,
+  //       [
+  //         {
+  //           text: 'Отмена',
+  //           style: 'cancel'
+  //         },
+  //         {
+  //           text: 'Удалить',
+  //           style: 'destructive',
+  //           onPress: () => {
+  //             setTodoId(null);
+  //             setTodos(prev => prev.filter(todo => todo.id !== id))
+  //           }
+  //         }
+  //       ],
+  //       {cancelable: false}
+  //   );
+  // }
 
   let content = (
-    <MainScreen
-      todos={todoContext.todos}
-      addTodo={addTodo}
-      removeTodo={removeTodo}
-      openTodo={setTodoId}
-    />
+      <MainScreen
+          todos={todos}
+          addTodo={addTodo}
+          removeTodo={removeTodo}
+          openTodo={changeScreen}
+      />
   )
 
   if (todoId) {
     const selectedTodo = todos.find(todo => todo.id === todoId)
     content = (
-      <TodoScreen
-        onRemove={removeTodo}
-        goBack={() => setTodoId(null)}
-        todo={selectedTodo}
-        onSave={updateTodo}
-      />
+        <TodoScreen
+            onRemove={removeTodo}
+            goBack={() => changeScreen((null))}
+            todo={selectedTodo}
+            onSave={updateTodo}
+        />
     )
   }
 
   return (
-    <View>
-      <Navbar title='Todo App!' />
-      <View style={styles.container}>{content}</View>
-    </View>
+      <View>
+        <Navbar title='Todo App' />
+        <View style={styles.container}>
+          {content}
+        </View>
+      </View>
   )
 }
 
@@ -89,4 +70,4 @@ const styles = StyleSheet.create({
     paddingHorizontal: THEME.PADDING_HORIZONTAL,
     paddingVertical: 20
   }
-})
+});

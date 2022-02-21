d563a3c6852bb74fa68bf65ffb901cc61b893134
dmaslov <dmaslov@neoflex.ru>
2022-02-21 13:35:32 +0300

    commit

--- src/context/todo/todoReducer.js
+++ src/context/todo/todoReducer.js
@@ -1,14 +1,20 @@
-import { ADD_TODO, REMOVE_TODO, UPDATE_TODO } from '../types'
+import {
+  ADD_TODO,
+  REMOVE_TODO,
+  UPDATE_TODO,
+  SHOW_LOADER,
+  HIDE_LOADER,
+  FETCH_TODOS,
+  CLEAR_ERROR,
+  SHOW_ERROR
+} from '../types'
 
 const handlers = {
-  [ADD_TODO]: (state, { title }) => ({
+  [ADD_TODO]: (state, { title, id }) => ({
     ...state,
     todos: [
       ...state.todos,
-      {
-        id: Date.now().toString(),
-        title
-      }
+      {id, title}
     ]
   }),
   [REMOVE_TODO]: (state, { id }) => ({
@@ -24,6 +30,11 @@ const handlers = {
       return todo
     })
   }),
+  [SHOW_LOADER]: state => ({...state, loading: true}),
+  [HIDE_LOADER]: state => ({...state, loading: false}),
+  [CLEAR_ERROR]: state => ({...state, loading: null}),
+  [SHOW_ERROR]: (state, {error}) => ({...state, error}),
+  [FETCH_TODOS]: (state, {todos}) => ({...state, todos}),
   DEFAULT: state => state
 }
 
